12b83bb5f3ae15ae5258b25735b4c165b17c4387
Pablo <pabloalanis_1@hotmail.com>
2020-07-20 14:39:18 -0300

    mejoras sobre mapa.js

--- src/assets/js/mapa.js
+++ src/assets/js/mapa.js
@@ -3,137 +3,11 @@ var map;
 map = L.map('map',{ attributionControl: false,center: [-45.8209,-67.5378],zoom: 11.5,minZoom: 11, maxZoom: 22,zoomControl: true});//.setView([-45.8209,-67.5378],11,5);
 var baseLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',maxZoom: 22,maxNativeZoom:19});
 baseLayer.addTo(map);
-baseLayer.on('load',loadGeoJSon);
-// Initialise the FeatureGroup to store editable layers
-//L.control.locate().addTo(map);//boton para geolocalizacion
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-var drawControl = new L.Control.Draw({
-  edit: {
-    featureGroup: drawnItems,
-  }
-});
-//map.addControl(drawControl);
-//
-
-//evento de agregar algo al mapa
-map.on(L.Draw.Event.CREATED, function (e) {
-  var type = e.layerType;
-  var layer = e.layer;
-  if (type==='marker') {
-    var datos=layer.getLatLng().toString();
-    //var radius = 100;
-    //L.circle(layer.getLatLng(), radius).addTo(map);
-    //alert('Soy un Marcador en '+layer.getLatLng().toString());
-    layer.bindTooltip(datos).openTooltip();
-    //aca deberia AGREGAR datos a mongo mediante node
-  }
-  //var infoPuntoLL=layer.getLatLng().toString();
-  // Do whatever else you need to. (save to db, add to map etc)
-  map.addLayer(layer);
-  drawnItems.addLayer(layer);
-});
-
-map.on('click', function(e) {
-//$('#modalPunto').modal('show');
-//$('.modalPunto-body').html(datos);
-/*Swal.fire(
-  'Good job!',
-  'You clicked the button!',
-  'success'
-)*/
-//$('.modal-body').html(layer.getLatLng().toString());
-  //L.marker(e.latlng).addTo(map);
-  //alert(e.latlng);
-     // e is an event object (MouseEvent in this case)
-})
-
-
-/*esto permite geolocalizarse (apiREST)
-map.on('click', function(e){
-  map.locate({setView:true,zoom:4,enableHighAccuracy:true});
-});
-function onLocationFound(e) {
-  var mkii = L.icon.mapkey({icon:"school",color:'#725139',background:'#f2c357',size:40});
-  var radius = e.accuracy / 2;
-  L.marker(e.latlng,{icon:mkii}).addTo(map).bindPopup("<h5>Si tu ubicación esta bien presiona sobre el circulo</h5>").openPopup();
-  var radio=L.circle(e.latlng,radius);
-  radio.addTo(map);
-  radio.on('click',function(e){
-    $('#modalCliente').modal('show');
-  });
-}
-
- function onLocationError(e) {
-   alert("Debes autorizar la geolocalización");
-  }
- map.on('locationfound',onLocationFound);
- map.on('locationerror',onLocationError);
- //esto permite geolocalizarse (apiREST)*/
-
-//geoJSON y renderizado en el mapa
-//datos desde mongo
-//var mapaMongo=$('#datoMongoose').html();
-//mapaMongo.JSON.stringify();
-//simulacion de geojson
-var punto={
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {"f1":"d"},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -67.50823974609375,
-          -45.868975197039205
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {"f1":"lcdetm"},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -67.52403259277344,
-          -45.882360730184025
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "f1": 11793,
-        "f2": "BT"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -67.50823974609375,
-          -45.87542933874844
-        ]
-      }
-    }
-  ]
-};
-
-
-//aca cargo/renderizo el geoJSON simple o con un each para cargar las propiedades en popup
-//L.geoJSON(punto).addTo(map);
-/*var layerGroup = L.geoJSON(punto, {
-  onEachFeature: function (feature, layer) {
-    //layer.bindPopup('<h1>'+feature.properties.f1+'</h1><p>name: '+feature.properties.f2+'</p>');
-  }
-}).addTo(map);*/
-
-
+//baseLayer.on('load',loadGeoJSon);
 
 function localizar(){
   //esto permite geolocalizarse (apiREST)
-
-    map.locate({setView:true,zoom:4,enableHighAccuracy:true});
+  map.locate({setView:true,zoom:4,enableHighAccuracy:true});
 
   function onLocationFound(e) {
     var mkii = L.icon.mapkey({icon:"school",color:'#725139',background:'#f2c357',size:40});
@@ -153,6 +27,8 @@ function localizar(){
    map.on('locationerror',onLocationError);
    //esto permite geolocalizarse (apiREST)
 }
+//fin localizar()
+
 //carga de geoJSON
 function loadGeoJSon(){
 
